c0103d35d4886bec3c69d3e42600731153a597bc
trungchien171 <trungchien171@gmail.com>
2024-09-06 10:45:03 +0700

    performance metrics

---
+++ performance.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def total_return(final_portfolio_value, initial_cap):
+    return (final_portfolio_value/initial_cap) - 1
+
+def annualized_return(total_return, num_of_days):
+    return np.power((1+total_return), 252/num_of_days) - 1
+
+def annualized_volatility(daily_returns):
+    return daily_returns.std() * np.sqrt(252)
+
+def sharpe_ratio(annualized_return, annualized_volatility, risk_free_rate=0):
+    try:
+        return (annualized_return - risk_free_rate) / annualized_volatility
+    except ZeroDivisionError:
+        return 0
+def sortino_ratio(daily_returns, annualized_return, risk_free_rate=0):
+    negative_returns = daily_returns[daily_returns < 0]
+    downside_volatility = negative_returns.std() * np.sqrt(252)
+    return (
+        (annualized_return - risk_free_rate) / downside_volatility
+        if downside_volatility > 0
+        else np.nan
+    )
+
+def maximum_drawdown(portfolio_values):
+    drawdown = portfolio_values / portfolio_values.cummax() - 1
+    return drawdown.min()
