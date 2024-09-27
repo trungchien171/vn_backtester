9e3678952c7dd11b7dfd4edad919aa815a351838
trungchien171 <trungchien171@gmail.com>
2024-09-27 09:43:56 +0700

    temporary pasteurization is only set as "False"

--- backtester/frontend.py
+++ backtester/frontend.py
@@ -109,7 +109,8 @@ if saved_settings['neutralization'] not in neutral:
 neutralization = st.sidebar.selectbox("Neutralization", neutral, index=neutral.index(saved_settings['neutralization']))
 decay = st.sidebar.slider("Decay", min_value=0, max_value=20, value=saved_settings['decay'])
 truncation = st.sidebar.text_input("Truncation", saved_settings['truncation'])
-pasteurization = st.sidebar.selectbox("Pasteurization", ['True', 'False'], index=['True', 'False'].index(saved_settings['pasteurization']))
+# pasteurization = st.sidebar.selectbox("Pasteurization", ['True', 'False'], index=['True', 'False'].index(saved_settings['pasteurization']))
+pasteurization = st.sidebar.selectbox("Pasteurization", ['False'], index=['False'].index(saved_settings['pasteurization']))
 delay = st.sidebar.selectbox("Delay", [0, 1])
 
 # Nút Apply để lưu cài đặt
