fa0bd97d7605b476c766031e0c763de59f95a69c
trungchien171 <trungchien171@gmail.com>
2024-10-02 15:39:19 +0700

    Update authentication.py

--- utils/authentication.py
+++ utils/authentication.py
@@ -7,7 +7,7 @@ import pandas as pd
 import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload, MediaIoBaseUpload
+from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
 
 def authenticate_gdrive():
     credentials = service_account.Credentials.from_service_account_info(
